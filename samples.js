6584825e2ead39478af8d078820bcecb5eaf62b7
Jeremy Archer <open-source@fatlotus.com>
2011-10-29 13:57:58 -0500

    Remove superflouous strokeStyle line.

--- samples.js
+++ samples.js
@@ -23,5 +23,5 @@
 
 GraphEditor.samples = [
 	null,
-	"draw = function() {\n  clearRect(0, 0, width, height);\n  \n  strokeStyle = 'rgba(100, 100, 100, 0.2)';\n  \n  for (var prd = 0; prd <= 10; prd++) {\n    if (prd == 5) {\n      strokeStyle = '#c00;';\n    } else if (prd == 10) {\n      strokeStyle = '#39c';\n    } else{\n      var r = prd * 30\n      strokeStyle = 'rgba('+r+','+r+','+r+',0.2)';\n    }\n    \n    beginPath();\n    \n    for (var x = 0; x < 500; x += 2) {\n      lineTo(x, (1 + Math.sin(x / (5 + 2 * prd)) * 0.2) * height / 2);\n    }\n    \n    stroke();\n    closePath();\n  }\n};"
+	"draw = function() {\n  clearRect(0, 0, width, height);\n  \n  for (var prd = 0; prd <= 10; prd++) {\n    if (prd == 5) {\n      strokeStyle = '#c00;';\n    } else if (prd == 10) {\n      strokeStyle = '#39c';\n    } else{\n      var r = prd * 30\n      strokeStyle = 'rgba('+r+','+r+','+r+',0.2)';\n    }\n    \n    beginPath();\n    \n    for (var x = 0; x < 500; x += 2) {\n      lineTo(x, (1 + Math.sin(x / (5 + 2 * prd)) * 0.2) * height / 2);\n    }\n    \n    stroke();\n    closePath();\n  }\n};"
 ]
